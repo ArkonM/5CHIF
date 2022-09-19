aea90c2d07b8a83c09d6f745425e948afe838d4a
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2022-09-19 21:31:29 +0200

    Hangman_(Unfertig, Kompiliert)
    Vielleicht die ganze check Funktion umschreiben

--- POS - SCW/C++/Konsole_Hangman/Konsole_Hangman/Konsole_Hangman.cpp
+++ POS - SCW/C++/Konsole_Hangman/Konsole_Hangman/Konsole_Hangman.cpp
@@ -7,46 +7,191 @@
 
 using namespace std;
 
+//Player health
+int lifesLeft = 8;
 
 string generateWord(string word_list[]) {
 
     int wordIndex;
     wordIndex = rand() % word_list->length();
-
     return word_list[ wordIndex ];
 }
 
 
 string checkWordSimilarity(string solution_word, string discovered_letters) {
 
-    bool word_found = false;
+    bool letter = false;
+    bool found_anything = false;
     string parts_found = "";
 
     for (int i = 0; i < solution_word.length(); i++) {
-        for (int y = 0; y < discovered_letters.length() && !word_found; y++) {
+        for (int y = 0; y < discovered_letters.length() && !letter; y++) {
             if (tolower(solution_word[i]) == tolower(discovered_letters[y])) {
                 discovered_letters.push_back(solution_word[i]);
                 parts_found.push_back(solution_word[i]);
                 parts_found.push_back(' ');
-                word_found = true;
+                letter = true;
             }
-            /*else if (!word_found) {
-                cout << "Der Buchstabe: " << discovered_letters[0] << " war leider falsch!" << endl;
-            }*/
         }
-        if (!word_found) {
+        if (!letter) {
             parts_found.append("_ ");
         }
         else {
-            word_found = false;
+            letter = false;
         }
+    }
+    if (!found_anything) {
+        lifesLeft -= 1;
     }
     return parts_found;
 }
 
 
+bool checkWin(string discovered_letters) {
+    
+    for (int i = 0; i < discovered_letters.size(); i++) {
+        if (discovered_letters[i] == '_') {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void zeroLifes() {
+    cout << "  ________" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |      o" << endl;
+    cout << "  |     \\|/" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |    _/ \_" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void oneLife() {
+    cout << "  ________" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |      o" << endl;
+    cout << "  |     \\|/" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void twoLifes() {
+    cout << "  ________" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |      o" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void threeLifes() {
+    cout << "  ________" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |      o" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void fourLifes() {
+    cout << "  ________" << endl;
+    cout << "  |      |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void fiveLifes() {
+    cout << "  ________" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void sixLifes() {
+    cout << "" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "  |" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void sevenLifes() {
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "=====" << endl;
+    cout << "|   |" << endl;
+    return;
+}
+
+void eightLifes() {
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    cout << "" << endl;
+    return;
+}
+
+
+void printHangman() {
+
+    switch (lifesLeft) {
+
+        case 0: zeroLifes();  break;
+        case 1: oneLife();    break;
+        case 2: twoLifes();   break;
+        case 3: threeLifes(); break;
+        case 4: fourLifes();  break;
+        case 5: fiveLifes();  break;
+        case 6: sixLifes();   break;
+        case 7: sevenLifes(); break;
+        case 8: eightLifes(); break;
+    }
+}
+
+
+//Hangman prints
+
+
+
 int main()
 {
+    srand(time(NULL));
+
     string inputText;
     string solution;
     string discoveredLetters;
@@ -54,15 +199,17 @@ int main()
     string wordList[] = { "Test", "Hangman", "Armin", "Sebi" };
     solution = generateWord(wordList);
 
-    while (true) {
+    printHangman();
+    
+
+    do {
         cin >> inputText;
-
+        system("cls");
         discoveredLetters = checkWordSimilarity(solution, inputText.append(discoveredLetters));
-
+        printHangman();
         cout << discoveredLetters << endl;
-        
-    }
-
+        cout << lifesLeft << endl;
+    } while (!checkWin(discoveredLetters));
 
 }
 
