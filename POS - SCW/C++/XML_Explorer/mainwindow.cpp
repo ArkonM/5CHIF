b5f7cf35baf8530dd4421bd83fcc8695e613d4d8
Armin-Schneider <58359803+Armin-Schneider@users.noreply.github.com>
2022-10-18 16:22:03 +0200

    UPDATE

--- POS - SCW/C++/XML_Explorer/mainwindow.cpp
+++ POS - SCW/C++/XML_Explorer/mainwindow.cpp
@@ -15,6 +15,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+QTreeWidgetItem* treeItem;
+
+void MainWindow::addTreeRoot(QString name){
+    treeItem = new QTreeWidgetItem(ui->XMLTree);
+    treeItem->setText(0, name);
+}
+
+void MainWindow::addTreeChild(QTreeWidgetItem* parent, QString name){
+    QTreeWidgetItem* treeItemChild = new QTreeWidgetItem();
+    treeItemChild->setText(0, name);
+    parent->addChild(treeItemChild);
+}
+
+
+//void MainWindow::AddTreeChildren(tinyxml2::XMLElement* e)
+
 
 void MainWindow::on_pushButton_clicked()
 {
@@ -22,12 +38,9 @@ void MainWindow::on_pushButton_clicked()
     QFileDialog dialog(this, "Datei wählen", "D:/Github/5CHIF/POS - SCW/C++/XML_Explorer");
     dialog.setNameFilter(tr("XML (*.xml)"));
     QStringList fileNames{};
+
     if (dialog.exec()){
         fileNames = dialog.selectedFiles();
-
-        ui->XMLTree->setColumnCount(1);
-        ui->XMLTree->addTopLevelItem();
-
     }
 }
 
